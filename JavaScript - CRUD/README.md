5ae694ebff1aab0eef1bd796fa4663097a3568bb
Anderson André <andersonmdn0@hotmail.com>
2021-04-20 13:47:53 -0300

    Course JS CRUD 100%

--- JavaScript - CRUD/README.md
+++ JavaScript - CRUD/README.md
@@ -15,13 +15,32 @@
 ![Badge](https://img.shields.io/badge/-GITHUB-000000?style=for-the-badge&logo=github&logoColor=ffffff)
 
 ## Aprendido durante o Curso / Learned during the Course
-- Fazer requisiÃ§Ãµes utilizando xmlhttprequest.
-- Lidar com promises.
-- Fazer uma requisiÃ§Ã£o http utilizando xmlhttprequest para buscar todos os clientes do servidor.
-- Refatorar o cÃ³digo utilizando promises melhorando a leitura do cÃ³digo.
-- Utilizar template literals para criar um template html.
-## RecomendaÃ§Ãµes
+- Fazer requisições utilizando **xmlhttprequest**.
+- Lidar com **promises**.
+- Fazer uma requisição http utilizando **xmlhttprequest** para buscar todos os clientes do servidor.
+- Refatorar o código utilizando **promises** melhorando a leitura do código.
+- Utilizar ```template literals``` para criar um template html.
 
+- Entender a ordem de execução do código JavaScript.
+- Puxar dados do servidor utilizando a função a **fetch api** ao invés do **xmlhttprequest** para modernizar o código.
+- Refatorar as responsabilidades do código pensando na manutenção da aplicação no futuro.
+
+- Usar a método ```closest``` para encontrar o elemento do ```DOM``` mais próximo ao que queremos remover
+- Criar um cliente utilizando o verbo http ```POST```.
+- Remover um elemento do dom com método ```remove()```
+- Deletar um cliente utilizando o verbo ```http DELETE```
+
+- Fazer uma query string utilizando a propriedade ```serachParams.get()``` para encontrar um id.
+- Criar uma IIFE ou função auto executável.
+- Editar os dados utilizando o verbo http ```PUT```.
+
+- Utilizar **async**/**await** para indicar funções assíncronas.
+- Tratar erros com **try**/**catch**
+
+## RecomendaÃ§Ãµes
+[Para saber mais: Promise.All - ALURA](https://cursos.alura.com.br/course/javascript-crud-assincrono/task/86449)
+[Funcionamento do Ambiente JavaScript](http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D)
+[O que é REST? - ALURA](https://cursos.alura.com.br/o-que-e-rest--c119)
 ## Rodando o Projeto
 1. Rodar o Json Server utilizando o **Yarn** ou **NPX**:
 
--- JavaScript - CRUD/service/cliente-service.js
+++ JavaScript - CRUD/service/cliente-service.js
@@ -1,31 +1,78 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-        <td class="td" data-td>${nome}</td>
-                    <td>${email}</td>
-                    <td>
-                        <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                        </ul>
-                    </td> 
-                    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-
-const tabela = document.querySelector('[data-tabela]')
-
-const listaClientes = () => {
+const listaClientes = () =>  {
     return fetch(`http://localhost:3000/profile`)
-    .then( resposta => {
-        return resposta.json()
+    .then(resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível listar os clientes')
     })
 }
 
-listaClientes()
-.then(data => {
-        data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-})})
+const criaCliente = (nome, email) => { 
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST', 
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possível criar um cliente')
+    })
+}
+
+const removeCliente = (id) => { 
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+    .then( resposta => { 
+        if(!resposta.ok){
+        throw new Error('Não foi possível deletar um cliente')
+        }
+    })
+}
+ 
+const detalhaCliente = (id) => { 
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => { 
+        if(resposta.ok){
+            return resposta.json()
+        }
+    
+        throw new Error('Não foi possível detalhar um cliente')
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: { 
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome, 
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível detalhar um cliente')
+    })
+}
+
+export const clienteService = { 
+    listaClientes,
+    criaCliente, 
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+
+}
