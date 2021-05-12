76980edb82c097473537aa55b52a38bfb4e016e1
andersonmdn <andersonmdn0@hotmail.com>
2021-05-12 20:35:11 -0300

    TypeScript Parte 1 63%

--- TypeScript - Parte 1/app/js/app.js
+++ TypeScript - Parte 1/app/js/app.js
@@ -1,2 +1,4 @@
-const negociacao = new Negociacao(new Date(), 1, 100);
-console.log(negociacao.quantidade);
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
